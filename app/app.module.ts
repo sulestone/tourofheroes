5426f9a8d4bfe8929d308527cf20e7213c92d09d
Sule Stone <sule.stone@cioxhealth.com>
2017-01-28 17:05:50 -0500

    added the logic for routing to heroes dashboard and hero detail

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,34 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule}    from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+  
+}
